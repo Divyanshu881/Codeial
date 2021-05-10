999055a721e280d4319d0325d4f519a54c17f953
Divyanshu881 <danshu.goodboy@gmail.com>
2021-05-10 20:37:23 +0530

    Setting up Views EJS

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,7 +1,11 @@
 module.exports.profile=function(req,res){
-    return res.end('<h1>User Profile</h1>');
+    return res.render('profile',{
+        title:"Profile"
+    });
 }
 
 module.exports.post=function(req,res){
-    return res.end('<h1>Post Profile</h1>');
+    return res.render('post',{
+        title:"Post"
+    });
 }
