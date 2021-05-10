f01ae66eab9a49ad866ca5428cee814e1e417f2a
Divyanshu881 <danshu.goodboy@gmail.com>
2021-05-10 20:15:14 +0530

    More controllers and routers

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,7 @@ const homeController=require('../controller/home_controller');
 console.log(`Router Loded`);
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+router.use('/users',require('./users'));
 
 module.exports=router;
