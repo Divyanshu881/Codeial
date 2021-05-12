c944067a47f971c8630fdd4cf6ab7491247a3627
Divyanshu881 <danshu.goodboy@gmail.com>
2021-05-12 18:13:22 +0530

    Sign Up changes Main

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-const express=require('express');
-const cookieParser=require('cookie-parser');
-const app=express();
-const port= 8000;
-
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 
 // use express router
-app.use('/',require('./routes'));
+app.use('/', require('./routes'));
 
-//setting up the view engine
-app.set('view engine','ejs');
-app.set('views','./views');
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-app.listen(port,function(err){
-    if(err){
-        console.log(`Error in runninf the server: ${err}`);
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
     }
 
     console.log(`Server is running on port: ${port}`);
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,16 @@
-const express= require('express');
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
 
-const usersController=require('../controller/users_controller');
+const usersController = require('../controllers/users_controller');
 
-router.get('/profile',usersController.profile);
+router.get('/profile', usersController.profile);
 
-//router.get('/post',usersController.post);
-
-router.get('/sign-up',usersController.signUp);
-router.get('/sign-in',usersController.signIp);
-
-router.post('/create',usersController.create);
-
-module.exports=router;
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
 
 
+router.post('/create', usersController.create);
+router.post('/create-session', usersController.createSession);
+
+
+module.exports = router;
