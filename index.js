ea29fdf1b66cbc15d1b4749703dad7c8858e4ddf
Divyanshu881 <danshu.goodboy@gmail.com>
2021-05-12 23:25:03 +0530

    Setup passport.js and authentication is done

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const { pass } = require('./config/mongoose');
 
 app.use(express.urlencoded());
 
@@ -17,13 +22,29 @@ app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
 
-// use express router
-app.use('/', require('./routes'));
+
 
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+
+app.use(session({
+    name: 'codeial',
+    //ToDo change secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie:{
+        maxAge: (1000*60*100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use express router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if (err){
